cf789200dbf94ef9a28f06d825c659bdcfc874b3
Keith <keithfernandes311#gmail.com>
2021-10-06 19:50:34 +0530

    ann_till_training

---
+++ src/utils/data_mgmt.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+def get_data(Validation_datasize):
+    mnist=tf.keras.datasets.mnist
+    (X_train_full,y_train_full),(X_test,y_test)=mnist.load_data() 
+    X_valid,X_train=X_train_full[:Validation_datasize]/255.,X_train_full[Validation_datasize:]/255.
+    y_valid,y_train=y_train_full[:Validation_datasize]/255.,y_train_full[Validation_datasize:]/255.
+
+    X_test=X_test/255.
+
+    return (X_train,y_train),(X_valid,y_valid),(X_test,y_test)
